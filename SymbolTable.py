094ab823b00b84423557704809bbca6635631e99
rivkaal <59804568+rivkaal@users.noreply.github.com>
2020-01-14 16:47:20 +0200

    Update SymbolTable.py

--- SymbolTable.py
+++ SymbolTable.py
@@ -4,6 +4,9 @@ class SymbolTable:
         self._type_dict = {}
         self._index_dict = {}
 
+    def __contains__(self, item):
+        if item in self._kind_dict:
+            return True
     def varCount(self, kind):
         count = 0
         for var in self._kind_dict:
@@ -12,6 +15,10 @@ class SymbolTable:
         return count
 
     def define(self, id_name, id_type, id_kind):
+        if id_kind == 'var':
+            id_kind = 'local'
+        if id_kind == 'field':
+            id_kind = 'this'
         self._index_dict[id_name] = self.varCount(id_kind)
         self._kind_dict[id_name] = id_kind
         self._type_dict[id_name] = id_type
@@ -30,4 +37,3 @@ class SymbolTable:
         self._kind_dict = {}
         self._type_dict = {}
         self._index_dict = {}
-
